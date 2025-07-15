2b718e84983ec82b2d9f229537bd1ab313cf2b1e
Zalog-hung <zalogfinal@gmail.com>
2025-07-15 17:52:53 +0700

    Update server.js

--- zalog/server.js
+++ zalog/server.js
@@ -1,11 +1,12 @@
+// server.js
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`✅ Server đang chạy tại cổng ${PORT}`);
 });
-
