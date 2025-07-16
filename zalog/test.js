d68b9137f2748ec89b2fee6dfc19b1e74cd9507b
Zalog-hung <zalogfinal@gmail.com>
2025-07-16 13:40:51 +0700

    Update test.js

--- zalog/test.js
+++ zalog/test.js
@@ -1,8 +1,1 @@
-function deleteRow(button) {
-    const actionCell = button.closest('.excel-cell');
-    const rowStartIndex = Array.from(gridElement.children).indexOf(actionCell) - formConfig.FORM_COLUMN_COUNT;
 
-    if (rowStartIndex >= 0) {
-        for (let i = formConfig.TOTAL_COLUMN_COUNT - 1; i >= 0; i--) {
-            gridElement.children[rowStartIndex + i].remove();}}}
- 
