d75021f6ee218a0d4e24069817cd4d282c02fdf2
Zalog-hung <zalogfinal@gmail.com>
2025-07-16 11:52:50 +0700

    Update main.js

--- zalog/main.js
+++ zalog/main.js
@@ -1,4 +1,4 @@
-✅ CẢI TIẾN: Gom tất cả các hằng số lên đầu để dễ quản lý.
+//✅ CẢI TIẾN: Gom tất cả các hằng số lên đầu để dễ quản lý.
 const KHACH_HANG_API_URL = "https://script.google.com/macros/s/AKfycbw6DcLseuze9340EK396D1JE9Of1qk0eyzQGd1Te19p0gnn-dwwioq1zS_1Iwe1WNY/exec";
 const LOG_API_URL = "https://script.google.com/macros/s/AKfycbwhGc1NHndpO2IYfEhFDFAiLHyTi1LqlWFSnfqtSxWPEQ5bCw7r4idZ23qvb83PitB0Dw/exec";
 const GHI_LOG_PROXY_URL = 'https://za-log-proxy-4pkb9hu3p-hung-za.vercel.app/api/proxy';
