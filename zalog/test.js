0d4f2e7f0f617627e98322671c52a4f9a0d9196f
Zalog-hung <zalogfinal@gmail.com>
2025-07-16 13:31:50 +0700

    Update test.js

--- zalog/test.js
+++ zalog/test.js
@@ -3,8 +3,6 @@ function deleteRow(button) {
     const rowStartIndex = Array.from(gridElement.children).indexOf(actionCell) - formConfig.FORM_COLUMN_COUNT;
 
     if (rowStartIndex >= 0) {
-        for (let i = 0; i < formConfig.TOTAL_COLUMN_COUNT; i++) {
-            gridElement.children[rowStartIndex].remove();
-        }
-    }
-}
+        for (let i = formConfig.TOTAL_COLUMN_COUNT - 1; i >= 0; i--) {
+            gridElement.children[rowStartIndex + i].remove();}}}
+ 
