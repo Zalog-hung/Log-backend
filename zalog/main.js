1eab123f4c32299e0265ac28f2581be3bd1b6156
Zalog-hung <zalogfinal@gmail.com>
2025-07-16 13:13:19 +0700

    Update main.js

--- zalog/main.js
+++ zalog/main.js
@@ -5,8 +5,7 @@ const GHI_LOG_PROXY_URL = 'https://za-log-proxy-4pkb9hu3p-hung-za.vercel.app/api
 const FORM_COLUMN_COUNT = 6;
 const LOG_COLUMN_COUNT = 26;
 const TOTAL_COLUMN_COUNT= 7;
-const FORM_COLUMN_COUNT= 6;
-const FIELDS_TO_KEEP_VALUE: [1, 4]; // Giữ lại giá trị "Khách Hàng" và "Ca"
+const FIELDS_TO_KEEP_VALUE= [1, 4]; // Giữ lại giá trị "Khách Hàng" và "Ca"
 const gridElement = document.querySelector('.excel-grid');
 
 let khachHangList = [];
