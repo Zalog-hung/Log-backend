6221d490f2c64e0893ada3c9c09f1ddd25f82afd
Zalog-hung <zalogfinal@gmail.com>
2025-07-16 16:34:22 +0700

    Update test.js

--- zalog/test.js
+++ zalog/test.js
@@ -1,1 +1,16 @@
+//Gắn Hàm DataCol Để Định Nghĩa Index tương ứng gọi trong HTLM
+
+function attachHandlersByDataCol() {
+  document.querySelectorAll('.excel-cell input[data-col]').forEach(input => {
+    const col = parseInt(input.dataset.col);
+
+    switch (col) {
+      case 0: index0(input); break;
+      case 1: index1(input); break;
+      // các cột khác sẽ thêm sau: index2(input), ...
+    }
+  });
+}
+
+attachHandlersByDataCol();
 
